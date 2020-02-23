bdb9fb3871d26f2b9afb54329da8af6f05cf5092
ofirisakov <ofirfifi@gmail.com>
2020-02-23 19:40:58 +0200

    change the way the feistel cipher is structured fix bugs where odd lengthed messages didn't work

--- feistel.py
+++ feistel.py
@@ -4,38 +4,45 @@ from itertools import cycle
 
 class FeistelCipher:
 	'''
-	data: The data to encrypt//decrypt
+	data: The data to encrypt/decrypt
 	key: The key to use in the encryption/decryption
 	balance_point: 
 	'''
-	def __init__ (self, data: bytes, key: bytes, balance_point: float=0.5):
+	def __init__(self, data: bytes, key: bytes, balance_point: float=0.5):
 		self.key = key
-		self.left = data[:int(balance_point * len(data))]
-		self.right = data[int(balance_point * len(data)):]
+		self.data = data
+		self.left_size = round(balance_point * len(data))
+		self.right_size = len(data) - self.left_size
 
 	'''
 	Function encrypts/decrypts the stored values by the Feistel Cipher laws
 	'''
 	def calculate(self):
-		# hash right
-		right_hash = hashlib.sha256()
-		right_hash.update(self.right)
-		right_hash.update(self.key)
-		hashed_right = right_hash.digest()
+		self.right_size, self.left_size = self.left_size, self.right_size
+		right = self.data[self.left_size:]
+		right_after_round = self.round_function(right, self.key)
 
-		# xor the values
-		xored_value = self.__xor(self.left, hashed_right)
+		left = self.data[:self.left_size]
+		xored_left = self.__bytes_xor(left, right_after_round)
 
 		# save outputs in inverted sides
-		self.left = self.right
-		self.right = xored_value
+		self.data = right + xored_left
 
-	def get_data(self):
-		return self.left + self.right
+	def round_function(self, data: bytes, key: bytes):
+		round_hash = hashlib.sha256()
+		round_hash.update(data)
+		round_hash.update(key)
+		return round_hash.digest()
+
+	def sawp_sides(self):
+		left = self.data[:self.right_size]
+		right = self.data[self.right_size:]
+		self.data = right + left
+		self.right_size, self.left_size = self.left_size, self.right_size
 
 	'''
 	Private function that XORs the given value with the given key
 	'''
-	def __xor(self, value_to_xor: bytes,  key_to_xor_with: bytes) -> bytes:
-		return bytes(value^key for value, key in zip(value_to_xor, cycle(key_to_xor_with)))
+	def __bytes_xor(self, value: bytes,  key: bytes) -> bytes:
+		return bytes(v^k for v, k in zip(value, cycle(key)))
 
