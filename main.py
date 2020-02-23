fc37c02669537f54b6318f09cf465790eb887b73
ofirisakov <ofirfifi@gmail.com>
2020-02-23 20:02:07 +0200

    add a better main to show the cipher

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 from feistel import FeistelCipher
 
 
-KEYS = {}  # these are the keys to the round function respectively, left side key right side balance point
+KEYS = {b'key': 0.5, b'key2': 0.5}  # these are the keys to the round function respectively, left side key right side balance point
 
 
 def main():
-	pass
-	# a = FeistelCipher(b'abcd', b'1', 0.5)
-	# print(a.get_data())
-	# a.calculate()
-	# print(a.get_data())
+	# this is how the network will work, just with more than 1 round
+	b = FeistelCipher(b'abcd', b'key')
+	print('data->', b.data)
+	b.calculate()
+	b.sawp_sides()
+	print('encrypted->', b.data)
+	b.calculate()
+	b.sawp_sides()
+	print('decrypted->', b.data)
 
-	# b = FeistelCipher(a.get_data(), b'2', 0.5)
-	# print(b.get_data())
-	# b.calculate()
-	# b.left, b.right = b.right, b.left
-	# print(b.get_data())
-	# b.calculate()
-	# print(b.get_data())
+	print()
 
-	# c = FeistelCipher(b.get_data(), b'1', 0.5)
-	# print(c.get_data())
-	# c.calculate()
-	# c.left, c.right = c.right, c.left
-	# print(c.get_data())
+	b = FeistelCipher(b'abcde', b'key')
+	print('data->', b.data)
+	b.calculate()
+	b.sawp_sides()
+	print('encrypted->', b.data)
+	b.calculate()
+	b.sawp_sides()
+	print('decrypted->', b.data)
 
 
 if __name__ == '__main__':
